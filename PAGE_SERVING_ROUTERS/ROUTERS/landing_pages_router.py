d60be6ddd6fbf5c314c17623f896ee7ab2218dbd
avijitbhuin21 <avijitbhuin21@gmail.com>
2025-12-26 15:07:15 +0530

    pushed linkedin and book changes

--- PAGE_SERVING_ROUTERS/ROUTERS/landing_pages_router.py
+++ PAGE_SERVING_ROUTERS/ROUTERS/landing_pages_router.py
@@ -1,14 +1,22 @@
 from fastapi import APIRouter
-from fastapi.responses import FileResponse
+from fastapi.responses import HTMLResponse
+from dotenv import load_dotenv
+import os
+    
+load_dotenv(override=True)
 
 router = APIRouter(prefix="/lp", tags=["Landing Pages"])
 
 
 @router.get("/linkedin-v1")
 async def get_linkedin_v1_page():
-    return FileResponse("PAGE_SERVING_ROUTERS/PAGES/linkedin_v1.html")
+    data = open("PAGE_SERVING_ROUTERS/PAGES/linkedin_v1.html", "r", encoding="utf-8").read()
+    data = data.replace("[[[linkedin_v1_url]]]", os.getenv("LINKEDIN_V1_URL", "https://calendly.com/suflex-media/linkedin-strategy-call"))
+    return HTMLResponse(content=data)
 
 
 @router.get("/book-v1")
 async def get_book_v1_page():
-    return FileResponse("PAGE_SERVING_ROUTERS/PAGES/book_v1.html")
+    data = open("PAGE_SERVING_ROUTERS/PAGES/book_v1.html", "r", encoding="utf-8").read()
+    data = data.replace("[[[book_v1_url]]]", os.getenv("BOOK_V1_URL", "https://calendly.com/sahil-suflexmedia/30min"))
+    return HTMLResponse(content=data)
